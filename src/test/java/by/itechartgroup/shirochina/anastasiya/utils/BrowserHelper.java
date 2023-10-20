c337b976d6e698f4d1c8d4dcd0ffe708b4365cf1
Anastasiya <shirochina16@gmail.com>
2023-10-20 12:55:06 +0300

    add BrowserHelper

--- src/test/java/by/itechartgroup/shirochina/anastasiya/utils/BrowserHelper.java
+++ src/test/java/by/itechartgroup/shirochina/anastasiya/utils/BrowserHelper.java
@@ -3,20 +3,20 @@ package by.itechartgroup.shirochina.anastasiya.utils;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Playwright;
-import org.apache.logging.log4j.Logger;
 
 public class BrowserHelper {
 
     public static Browser getBrowserSetting(Playwright playwright) {
-        Browser browser = null;
-        if (PropertiesHelper.getBrowser().equals("chrome")) {
-            return browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(PropertiesHelper.getBrowserHeadless()));
-        } else if (PropertiesHelper.getBrowser().equals("firefox")) {
-            return browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(PropertiesHelper.getBrowserHeadless()));
-        } else if (PropertiesHelper.getBrowser().equals("webkit")) {
-            return browser = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(PropertiesHelper.getBrowserHeadless()));
+        BrowserType.LaunchOptions launchOptions = new BrowserType.LaunchOptions().setHeadless(PropertiesHelper.getBrowserHeadless());
+        String browserName = PropertiesHelper.getBrowser();
+        if (browserName.equals("chrome")) {
+            return playwright.chromium().launch(launchOptions);
+        } else if (browserName.equals("firefox")) {
+            return playwright.firefox().launch(launchOptions);
+        } else if (browserName.equals("webkit")) {
+            return playwright.webkit().launch(launchOptions);
         } else {
-            return browser;
+            return null;
         }
     }
 }
